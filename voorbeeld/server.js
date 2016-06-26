e7ad8d3b5cd5fc6f59bdd9bb56840bfcab1dac9d
Kees Zijlmans <kpzijlma@avans.nl>
2016-06-26 19:34:56 +0200

    fix

--- voorbeeld/server.js
+++ voorbeeld/server.js
@@ -7,14 +7,60 @@ var connect = require('connect');
 var os=require('os');
 var ifaces=os.networkInterfaces();
 var err;
-var verwarmingStatus = "I";
-var ketelStatus= "I";
-var temp;
+var verwarmingStatus = "ON";
+var ketelStatus= "ON";
+var temp ="tempratuur:??";
 gpio.setup(16, gpio.DIR_OUT);
 gpio.setup(18, gpio.DIR_OUT);
+function getWebsite(req,resp){
+	var sb = new StringBuilder({newline: "\r\n"});
+	
+	sb.appendLine("<html> ");
+	sb.appendLine("<body>");
+	sb.appendLine("<p>Verwarming</p>");
+	sb.appendLine("<input type='button' onclick='setVerwarming()'  id='verwarming' value="+verwarmingStatus+"></input>");
+	sb.appendLine("<p>Ketel</p>");
+	sb.appendLine("<input type='button' onclick='setKetel()' id='ketel' value="+ketelStatus+"></input>");
+	sb.appendLine("<p>Tempratuur: "+temp+"°C</p>");
+	sb.appendLine("<input type='text' id='autoTempField'></input>");
+	sb.appendLine("<input type='button' onClick='setAutoTemp()' id='setTemp' value='Set Automatic Tempratuur in Celcius'></input>");
+	sb.appendLine("</body>");
+	sb.appendLine("<script type='text/javascript'>");
+	sb.appendLine("var IPAddress='192.168.0.21';");
+	sb.appendLine("var start");
+	sb.appendLine("var end;");
+	sb.appendLine("var currentPageUrl = '';");
+	//sb.appendLine("var myVar = setInterval(myTimer, 1000);");
+	sb.appendLine("if (typeof this.href === 'undefined') {");
+	sb.appendLine("currentPageUrl = document.location.toString().toLowerCase();");
+	sb.appendLine("}");
+	sb.appendLine("else {");
+	sb.appendLine("currentPageUrl = this.href.toString().toLowerCase();");
+	sb.appendLine("}");
+	sb.appendLine("start=currentPageUrl.search('http://')+7;");
+	sb.appendLine("end=currentPageUrl.search(':80');");
+	sb.appendLine("IPAddress=currentPageUrl.substring(start,end);");
+	sb.appendLine("function setKetel() {");
+	sb.appendLine("location.href = 'http://'+IPAddress+':8080?cmd=KetelSwitch';");
+	sb.appendLine("};");
+	sb.appendLine("function setVerwarming() {");
+	sb.appendLine("location.href = 'http://'+IPAddress+':8080?cmd=VerwarmingSwitch';");
+	sb.appendLine("};");
+	sb.appendLine("function setAutoTemp(){");
+	sb.appendLine("location.href = 'http://'+IPAddress+':8080?cmd='+tempField.text;");
+	sb.appendLine("};");
+	sb.appendLine("</script>");
+	sb.appendLine("</html>");
+	
+	sb.build(function(err,result){
+		resp.writeHead(200,{"Content-Type":"text/html"});
+		resp.write(result);
+		resp.end();
+	}
+}
 
  function VerwarmingAan() {
-	 verwarmingStatus = "I";
+	 verwarmingStatus = "ON";
     gpio.write(16, true, function(err) {
         if (err) console.log('Error writing to pin 16 true');
         console.log('Written to pin');
@@ -23,7 +69,7 @@ gpio.setup(18, gpio.DIR_OUT);
 
 
 function VerwarmingUit() {
-	verwarmingStatus = "O";
+	verwarmingStatus = "OFF";
     gpio.write(16, false, function(err) {
         if (err) console.log('Error writing to pin 16 false');
         console.log('Written to pin');
@@ -31,7 +77,7 @@ function VerwarmingUit() {
 }
 
 function KetelAan(){
-		ketelStatus= "I";
+		ketelStatus= "ON";
         gpio.write(18,true,function(err){
         if(err) console.log('Error writing to pin 18 true');
         console.log('Writing to pin');
@@ -39,7 +85,7 @@ function KetelAan(){
 }
 
 function KetelUit(){
-		ketelStatus= "O";
+		ketelStatus= "OFF";
         gpio.write(18,false,function(err){
         if(err) console.log('Error writing to pin 18 false');
         console.log('Writing to pin');
@@ -92,9 +138,10 @@ http.createServer(function (request, response) {
 			if(id=="Temperatuur"){
 				Temperatuur();
 			}
-            response.writeHead(200);
-            response.write("<script type='text/javascript'>location.href = 'http://"+IPAddress+":8000ans="+ketelStatus+verwarmingStatus+temp"'</script>");
-            response.end();
+			getWebsite(request,response);
+            //response.writeHead(200);
+            //response.write("<script type='text/javascript'>location.href = 'http://"+IPAddress+":8000ans="+ketelStatus+""+verwarmingStatus+""+temp+"'</script>");
+            //response.end();
         }
     }).listen(8080,IPAddress);
 
